fdd9b9c9a6620288100c72512352a9406726bc23
Raqeeb Hamza <raqeeb.hamza@funavry.com>
2022-03-14 18:01:23 +0500

    created cycle in graph and find loop in linkedList

--- utils.py
+++ utils.py
@@ -23,6 +23,11 @@
 
 #solution:3  using l pointer and R pointer
 
+from operator import contains
+from platform import node
+import turtle
+
+
 def twoNumberSum(arr,targetSum): # O(nlogn) time | O(1) space
     arr.sort()
     left=0
@@ -296,6 +301,287 @@ def getKnapSackItems(knapsackValues,items):
             break
     return list(reversed(sequence))    
 
+#Q.11 trapwater rain...........................................................................
+def trap(height): # O(N) time | O(1) space
+    leftMax,rightMax=0,0
+    left,right=0,len(height)-1
+    trappedWater=0
+    while left<right:
+        if height[left] > leftMax:
+            leftMax=height[left]
+        if height[right]> rightMax:
+            rightMax=height[right]
+        if leftMax<rightMax:
+            trappedWater+=max(0,leftMax-height[left])
+            left+=1
+        else:
+            trappedWater+=max(0,rightMax-height[right])
+            right-=1
+    return trappedWater  
+
+#Q.12 levenshteinDistance........................................................................
+# def levenshteinDistance(str1,str2): # O(nm) Time | O(nm) space
+#     edits=[[ x for x in range(len(str1)+1)] for y in range(len(str2)+1)] 
+#     for i in range(1,len(str2)+1):
+#         edits[i][0]=edits[i-1][0]+1
+#     for i in range(1,len(str2)+1):
+#         for j in range(1,len(str1)+1):
+#             if str1[j-1] == str2[i-1]:
+#                 edits[i][j]=edits[i-1][j-1]
+#             else:
+#                 edits[i][j]=1+min(edits[i][j-1],edits[i-1][j],edits[i-1][j-1]) 
+#     return edits[-1][-1]
+# 2nd solution with better space complexity------------------------------------------------------------
+def levenshteinDistance(str1,str2): # O(nm) time | O(min(n,m)) Space
+    small=str1 if len(str1)<len(str2) else str2
+    big=str1 if len(str1)>=len(str2) else str2
+    evenEdits=[x for x in range(len(small)+1)]
+    oddEdits=[None for  x in range(len(small)+1)]
+    for i in range(1,len(big)+1):
+        if i%2==1:
+            currentEdits=oddEdits
+            previousEdits=evenEdits
+        else:
+            currentEdits=evenEdits
+            previousEdits=oddEdits
+        currentEdits[0]=i 
+        for j in range(1,len(small)+1):
+            if big[i-1]==small[j-1]:
+                currentEdits[j]=previousEdits[j-1]
+            else:
+                currentEdits[j]=1+min(currentEdits[j-1],previousEdits[j],previousEdits[j-1])
+    return evenEdits[-1] if len(big)%2==0 else oddEdits[-1]
+
+#Q.14 RiverSizes ---------------------------------------------------------------------
+def riverSizes(matrix):  # O(wh) time | O(wh) space
+    sizes=[]
+    visited=[[False for value in row] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if visited[i][j]:
+                continue
+            else:
+                traverseNode(i,j,matrix,visited,sizes)
+    return sizes
+def traverseNode(i,j,matrix,visited,sizes):
+    currnentSize=0
+    nodesToExplore=[[i,j]] # stack
+    while len(nodesToExplore):
+        currnentNode=nodesToExplore.pop()
+        i=currnentNode[0]
+        j=currnentNode[1]
+        if visited[i][j]:
+            continue
+        visited[i][j]=True
+        if matrix[i][j]==0:
+            continue
+        currnentSize+=1
+        unvisitedNeighbors=getUnVisitedNeighbors(i,j,matrix,visited)
+        for neigbors in unvisitedNeighbors:
+            nodesToExplore.append(neigbors)
+    if currnentSize>0:
+        sizes.append(currnentSize)
+
+def getUnVisitedNeighbors(i,j,matrix,visited):
+    neighbors=[]
+    if i!=0:  # up neighbor
+        if not visited[i-1][j]:
+            neighbors.append([i-1,j])
+    if j!=0: # left neighbor
+        if not visited[i][j-1]: 
+            neighbors.append([i,j-1])
+    if i!=len(matrix)-1: # down neighbor
+        if not visited[i+1][j]:
+            neighbors.append([i+1,j])
+    if j!= len(matrix[i])-1: # right neighbor
+        if not visited[i][j+1]:        
+            neighbors.append([i,j+1])
+    return neighbors
+
+#Q.15 Apartment Hunting ----------------------------------------------------------------
+def apartmentHunting(blocks,req):
+    minDistancesFromBlocks=list(map(lambda req:getMinDistances(blocks,req),req))
+    print(minDistancesFromBlocks)
+    maxDistancesAtBlocks=getMaxDistanceAtBlocks(blocks,minDistancesFromBlocks)
+    return getIdxOfMinValue(maxDistancesAtBlocks)
+def getMaxDistanceAtBlocks(blocks,minDistanceFromBlocks):
+    maxDistanceAtBlocks=[0 for block in blocks]
+    for i in range(len(blocks)):
+        minDistancesAtBlock=list(map(lambda distances:distances[i],minDistanceFromBlocks))
+        maxDistanceAtBlocks[i]=max(minDistancesAtBlock)
+    return maxDistanceAtBlocks
+def getMinDistances(blocks,req):
+    minDistances=[0 for block in blocks]
+    closestReqIdx=float("inf")
+    for i in range(len(blocks)):
+        if blocks[i][req]:
+            closestReqIdx=i
+        minDistances[i]=distanceBetween(i,closestReqIdx)
+    for i in reversed(range(0,len(blocks))):
+        if blocks[i][req]:
+            closestReqIdx=i
+        minDistances[i]=min(minDistances[i],distanceBetween(i,closestReqIdx))    
+    return minDistances
+def distanceBetween(a,b):
+    return abs(a-b)        
+def getIdxOfMinValue(array):
+    idxAtMinValue=0
+    minValue=float("inf")
+    for i in range(len(array)):
+        currVal=array[i]
+        if currVal<minValue:
+            minValue=currVal
+            idxAtMinValue=i
+    return idxAtMinValue    
+
+
+#Q.16 Cycle in Graph -------------------------------------------------------------------
+# def cycleInGraph(edges): # O(v+e) Time | O(v) space
+#     numberOfNodes=len(edges)
+#     nodesInStack=[False for _ in range(numberOfNodes)]
+#     isVisited=[False for _ in range(numberOfNodes)]
+#     for node in range(numberOfNodes):
+#         if isVisited[node]:
+#             continue
+#         containsCycle=isNodeContainsCycle(edges,node,isVisited,nodesInStack)
+#         if containsCycle:
+#             return True
+#     return False
+# def isNodeContainsCycle(edges,node,isVisited,nodesInStack):
+#     nodesInStack[node]=True
+#     isVisited[node]=True
+#     neigbours=edges[node]
+#     for neighbor in neigbours:
+#         if not isVisited[neighbor]:
+#             containsCycle=isNodeContainsCycle(edges,neighbor,isVisited,nodesInStack)
+#             if containsCycle:
+#                 return True
+#         elif nodesInStack[neighbor]:
+#             return True   
+#     nodesInStack[node]=False         
+#     return False
+#Solution:2.....................................................................
+WHITE, GREY,BLACK=0,1,2
+def cycleInGraph(edges): # O(v+e) Time | O(v) space 
+    numberOfNodes=len(edges)
+    colors=[WHITE for _ in range(numberOfNodes)]
+    for node in range(numberOfNodes):
+        if colors[node]!=WHITE:
+            continue
+        containsCycle=traverseAndColorNode(edges,node,colors)
+        if containsCycle:
+            return True
+    return False
+def traverseAndColorNode(edges,node,colors):
+    colors[node]=GREY
+    neighbors=edges[node]
+    for neighbor in neighbors:
+        if colors[neighbor]==GREY:
+            return True
+        if colors[neighbor]!=WHITE:
+            continue
+        containsCycle=traverseAndColorNode(edges,neighbor,colors)
+        if containsCycle:
+            return True
+    colors[node]=BLACK
+    return False        
+
+#Q.16 Loop in the linkedList -----------------------------------------------------------
+class LinkedList:
+    def __init__(self,value) -> None:
+        self.value=value
+        self.next=None
+def findLoop(head:LinkedList): # Find loop and return loop head of the linkedList
+    if not head:
+        return head
+    first=head.next
+    second=head.next.next
+    while first!=second:
+        first=first.next
+        second=second.next.next
+    first=head
+    while first!=second:
+        first=first.next
+        second=second.next
+    return first
+
+
+
+
+
+
+
+#Interviews Questions--------------------------------------------------------------------
+
+# Zalando Questions---------------------------------------------------------------------
+# B=['X.....>', '..v..X.', '.>..X..', 'A......']
+# B=['...', '>.A']
+# B=['A.v', '...']
+def AssasinVsGuards(B):
+    matrix=[]
+    for i in range(0,len(B)):
+        matrix.append(list(B[i]))   
+    up='^'
+    down='v'
+    right='>'
+    left='<'
+    ai,aj=0,0
+    visited=[[False for value in row] for row in matrix]
+    for i in range(0,len(matrix)):
+        for j in range(0,len(matrix[i])):
+            if matrix[i][j]=='A':
+                ai=i
+                aj=j
+            if matrix[i][j]==up:
+                for k in reversed(range(0,i-1)):
+                    if matrix[k][j]=='.':
+                        matrix[k][j]=up
+                    else:
+                        break
+            if matrix[i][j]==down:
+                for k in range(i+1,len(matrix)):
+                    if matrix[k][j]=='.':
+                        matrix[k][j]=down
+                    else:
+                        break
+            if matrix[i][j]==right:
+                for k in range(j+1,len(matrix[i])):
+                    if matrix[i][k]=='.':
+                        matrix[i][k]=right
+                    else:
+                        break
+    q=[[ai,aj]]
+    if matrix[len(matrix)-1][len(matrix[0])-2]==right:
+        return False
+    if matrix[len(matrix)-2][len(matrix[0])-1]==down:
+        return False    
+    count=1
+    while len(q):
+        curr=q.pop(0)
+        visited[curr[0]][curr[1]]=True
+        if curr[0]==len(matrix)-1 and curr[1]==len(matrix[0])-1:
+            return True
+        neigbors=getneighbors(curr[0],curr[1],matrix,visited)
+        for n in neigbors:    
+            q.append(n)  
+    return False    
+
+def getneighbors(i,j,matrix,visited):
+    neigbors=[]
+    if i!=0:  # up neighbor
+        if matrix[i-1][j]=='.' and visited[i-1][j]:
+            neigbors.append([i-1,j])
+    if j!=0: # left neighbor
+        if  matrix[i][j-1]=='.' and  visited[i][j-1]: 
+            neigbors.append([i,j-1])
+    if i!=len(matrix)-1: # down neighbor
+        if  matrix[i+1][j]=='.' and visited[i+1][j]:
+            neigbors.append([i+1,j])
+    if j!= len(matrix[i])-1: # right neighbor
+        if  matrix[i][j+1]=='.' and visited[i][j+1]:        
+            neigbors.append([i,j+1])
+    return neigbors
+
 
 #----------------function calling---------------
 
@@ -330,9 +616,86 @@ def getKnapSackItems(knapsackValues,items):
 
 # print(sameBsts([10, 8, 5, 15, 2, 12, 11, 94, 81],[10, 15, 8, 12, 94, 81, 5, 2, 11]))
 
-print(knapsackProblem([
-  [1, 2],
-  [4, 3],
-  [5, 6],
-  [6, 7]
-],10))
+# print(knapsackProblem([
+#   [1, 2],
+#   [4, 3],
+#   [5, 6],
+#   [6, 7]
+# ],10))
+
+# print(trap([4,2,0,3,2,5]))
+
+# print(levenshteinDistance("abc","yabd"))
+
+# print(riverSizes([
+#   [1, 0, 0, 1, 0],
+#   [1, 0, 1, 0, 0],
+#   [0, 0, 1, 0, 1],
+#   [1, 0, 1, 0, 1],
+#   [1, 0, 1, 1, 0]
+# ]))
+ 
+# print(apartmentHunting([
+#   {
+#     "gym":False ,
+#     "school": True,
+#     "store": False
+#   },
+#   {
+#     "gym": True,
+#     "school": False,
+#     "store": False
+#   },
+#   {
+#     "gym": True,
+#     "school": True,
+#     "store": False
+#   },
+#   {
+#     "gym": False,
+#     "school": True,
+#     "store": False
+#   },
+#   {
+#     "gym": False,
+#     "school": True,
+#     "store": True
+#   }],["gym", "school", "store"]))
+
+
+# print(cycleInGraph([
+#   [1, 3],
+#   [2, 3, 4],
+#   [0],
+#   [],
+#   [2, 5],
+#   []
+# ]))
+
+# print(findLoop({
+#   "head": "0",
+#   "nodes": [
+#     {"id": "0", "next": "1", "value": 0},
+#     {"id": "1", "next": "2", "value": 1},
+#     {"id": "2", "next": "3", "value": 2},
+#     {"id": "3", "next": "4", "value": 3},
+#     {"id": "4", "next": "5", "value": 4},
+#     {"id": "5", "next": "6", "value": 5},
+#     {"id": "6", "next": "7", "value": 6},
+#     {"id": "7", "next": "8", "value": 7},
+#     {"id": "8", "next": "9", "value": 8},
+#     {"id": "9", "next": "4", "value": 9}
+#   ]
+# }))
+
+
+
+
+
+
+
+
+
+# Interview Question Calls
+# print(AssasinVsGuards(['X.....>', '..v..X.', '....X..', 'A......']))               
+
